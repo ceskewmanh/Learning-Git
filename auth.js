c8b24623989242deabdffa047ccba477f155b94a
Colby Eskew <ceskew@manh.com>
2023-12-20 09:33:26 -0500

    Fixed Text

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 function auth(login, password){
     if (login == 'login' &&password == "password"){
-        return "Lookin good!";
+        return "Welcome!";
     }else{
         return"Liar";
     }
